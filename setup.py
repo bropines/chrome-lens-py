e6a00773a36d469db0e8801a16405e02bf04f8b0
Sergey Pinus <bropines@gmail.com>
2024-08-11 21:40:58 +0500

    Update setup for pypi actions

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
     name='chrome_lens_py',
@@ -18,10 +25,12 @@ setup(
             'lens_scan=chrome_lens_py.main:main',
         ],
     },
-    description='Library to use Google Lens OCR for free, via API used in Chromium.',
+    description='Library to use Google Lens OCR via API used in Chromium.',
+    long_description=README,
+    long_description_content_type='text/markdown',  # Указание типа содержимого
     author='Bropines',
     author_email='bropines@gmail.com',
-    url='https://github.com/bropines/chrome-lens-api-py',
+    url='https://github.com/bropines/chrome-lens-py',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
