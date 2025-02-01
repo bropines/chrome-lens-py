7facee7f41931885b9d1ae3fa0ed0b05064d059f
Curlypla <98696775+Curlypla@users.noreply.github.com>
2025-02-01 18:28:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import pathlib
 HERE = pathlib.Path(__file__).parent
 
 # The text of the README file
-README = (HERE / "README.md").read_text()
+README = (HERE / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='chrome_lens_py',
