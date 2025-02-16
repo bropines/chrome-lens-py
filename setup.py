dbaf523e8fe2ab43bb0fd01f0466c1f973508233
Sergey Pinus <57861007+bropines@users.noreply.github.com>
2025-02-16 23:26:21 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='chrome_lens_py',
-    version='2.0.0',
+    version='2.0.1',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     python_requires='>=3.8',
